976ce3b2da4a4ebd8d40d9253f206528653581ff
mogh99 <mohammedameenha@gmail.com>
2022-08-14 13:26:32 -0400

    Complete the favorite grid functionality

--- qubes_menu/favorites_page.py
+++ qubes_menu/favorites_page.py
@@ -37,19 +37,19 @@ from gi.repository import Gtk
 logger = logging.getLogger('qubes-appmenu')
 
 LIST_WHITE_ICON = Gtk.Image.new_from_pixbuf(
-    load_icon(constants.LIST_WHITE, Gtk.IconSize.LARGE_TOOLBAR)
+    load_icon(constants.LIST_WHITE, Gtk.IconSize.DND)
 )
     
 LIST_BLACK_ICON = Gtk.Image.new_from_pixbuf(
-    load_icon(constants.LIST_BLACK, Gtk.IconSize.LARGE_TOOLBAR)
+    load_icon(constants.LIST_BLACK, Gtk.IconSize.DND)
 )
 
 GRID_WHITE_ICON = Gtk.Image.new_from_pixbuf(
-    load_icon(constants.GRID_WHITE, Gtk.IconSize.LARGE_TOOLBAR)
+    load_icon(constants.GRID_WHITE, Gtk.IconSize.DND)
 )
 
 GRID_BLACK_ICON = Gtk.Image.new_from_pixbuf(
-    load_icon(constants.GRID_BLACK, Gtk.IconSize.LARGE_TOOLBAR)
+    load_icon(constants.GRID_BLACK, Gtk.IconSize.DND)
 )
 
 class FavoritesPage:
@@ -66,71 +66,64 @@ class FavoritesPage:
         self.vm_manager = vm_manager
         self.fav_apps_layout = read_settings(constants.FAVORITE_APPS_LAYOUT)
 
-        self.fav_apps_view: Gtk.Viewport = builder.get_object('fav_view_port')
         self.fav_layout_toggle: Gtk.Button = builder.get_object('fav_layout_toggle')
 
-        self.fav_layout_toggle.set_image(LIST_WHITE_ICON)\
+        self.fav_layout_toggle.set_image(GRID_WHITE_ICON)\
             if self.fav_apps_layout == constants.LIST \
-                else self.fav_layout_toggle.set_image(GRID_WHITE_ICON)
+                else self.fav_layout_toggle.set_image(LIST_WHITE_ICON)
 
         self.fav_layout_toggle.connect('clicked', self._toggle_layout)
         self.fav_layout_toggle.connect('enter-notify-event', self._enter_layout_button)
         self.fav_layout_toggle.connect('leave-notify-event', self._leave_layout_button)
 
-        self.app_grid: Gtk.FlowBox = Gtk.FlowBox(orientation=Gtk.Orientation.HORIZONTAL)
-        self.app_grid.get_style_context().add_class('left_pane')
-        self.app_grid.set_halign(Gtk.Align.START)
-        self.app_grid.set_valign(Gtk.Align.START)
-        self.app_grid.set_min_children_per_line(2)
-        self.app_grid.set_max_children_per_line(3)
+        self.app_grid: Gtk.FlowBox = builder.get_object('fav_flow_box')
         self.app_grid.connect('child-activated', self._app_clicked_grid)
         self.app_grid.set_sort_func(
-            lambda x, y: x.get_child().app_info.app_name > y.get_child().app_info.app_name
+            lambda x, y: str(x.get_child().app_info.vm) > str(y.get_child().app_info.vm)
         )
         self.app_grid.invalidate_sort()
 
-        self.app_grid.show_all()
         self.app_grid.set_selection_mode(Gtk.SelectionMode.NONE) 
 
-
-        self.app_list: Gtk.ListBox = Gtk.ListBox()
-        self.app_list.get_style_context().add_class('left_pane')
+        self.app_list: Gtk.ListBox = builder.get_object('fav_list_box')
         self.app_list.connect('row-activated', self._app_clicked_list)
         
         self.app_list.set_sort_func(
-            lambda x, y: x.app_info.app_name > y.app_info.app_name)
+            lambda x, y: str(x.app_info.vm) > str(y.app_info.vm)
+        )
         
-        self.app_list.show_all()
         self.app_list.invalidate_sort()
         self.app_list.set_selection_mode(Gtk.SelectionMode.NONE)        
         
-        self.fav_apps_view.add(self.app_list)\
-            if self.fav_apps_layout == constants.LIST \
-                else self.fav_apps_view.add(self.app_grid)
-
         self.desktop_file_manager.register_callback(self._app_info_callback)
+
+        self.app_list.show_all()\
+            if self.fav_apps_layout == constants.LIST \
+                else self.app_grid.show_all()
+        
 
         self.dispatcher.add_handler(
             f'domain-feature-set:{constants.FAVORITES_FEATURE}',
             self._feature_set)
         self.dispatcher.add_handler('domain-add', self._domain_added)
         self.dispatcher.add_handler('domain-delete', self._domain_deleted)
+        
 
     def _toggle_layout(self, *args, **kwargs):
         if self.fav_apps_layout == constants.LIST:
-            self.fav_apps_view.remove(self.app_list)
-            self.fav_apps_view.add(self.app_grid)
+            self.app_list.hide()
+            self.app_grid.show_all()
 
-            self.fav_layout_toggle.set_image(GRID_WHITE_ICON)
+            self.fav_layout_toggle.set_image(LIST_WHITE_ICON)
             
             self.fav_apps_layout = constants.GRID
             write_settings(constants.FAVORITE_APPS_LAYOUT, constants.GRID)
             
         elif self.fav_apps_layout == constants.GRID:
-            self.fav_apps_view.remove(self.app_grid)
-            self.fav_apps_view.add(self.app_list)
+            self.app_grid.hide()
+            self.app_list.show_all()
 
-            self.fav_layout_toggle.set_image(LIST_WHITE_ICON)
+            self.fav_layout_toggle.set_image(GRID_WHITE_ICON)
 
             self.fav_apps_layout = constants.LIST
             write_settings(constants.FAVORITE_APPS_LAYOUT, constants.LIST)
@@ -189,9 +182,12 @@ class FavoritesPage:
 
     def _add_from_app_info(self, app_info):
         list_entry = FavoritesAppListEntry(app_info, self.vm_manager)
+
         grid_entry = FavoritesAppGridEntry(app_info, self.vm_manager)
+
         app_info.entries.append(list_entry)
         app_info.entries.append(grid_entry)
+
         self.app_list.add(list_entry)
         self.app_grid.add(grid_entry)
 
@@ -239,12 +235,9 @@ class FavoritesPage:
 
                     list_child.app_info.entries.remove(list_child)
                     grid_child.get_child().app_info.entries.remove(grid_child.get_child())
-
                     self.app_list.remove(list_child)
                     self.app_grid.remove(grid_child)
 
-                    self.app_list.show_all()
-                    self.app_grid.show_all()
                     break
             
         # Add a favorite app
@@ -256,7 +249,9 @@ class FavoritesPage:
                     self._add_from_app_info(app_info)
                     break
 
-        
+        # (self.app_list.show_all() and self.app_grid.hide())\
+        #     if self.fav_apps_layout == constants.LIST \
+        #         else (self.app_list.hide() and self.app_grid.show_all())
 
     def _domain_added(self, _submitter, _event, vm, **_kwargs):
         """On a newly created domain, load all favorites from features
