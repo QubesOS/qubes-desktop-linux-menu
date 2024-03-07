7248afd3051646c9383105612492ea9d622bc4f9
Marta Marczykowska-Górecka <marmarta@invisiblethingslab.com>
2024-03-07 19:16:33 +0100

    Move tests to new Mock Qubes object from core-admin-client
    More in-line with other tests.

--- qubes_menu/tests/conftest.py
+++ qubes_menu/tests/conftest.py
@@ -19,50 +19,9 @@
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import pytest
-import unittest.mock
-from qubesadmin.tests import TestVM, TestVMCollection
-
-
-class TestApp(object):
-    def __init__(self):
-        self.domains = TestVMCollection(
-            [
-                ('dom0', TestVM('dom0')),
-            ]
-        )
-        self.log = unittest.mock.Mock()
-
-    def _invalidate_cache(self, *_args, **_kwargs):
-        pass
+from qubesadmin.tests.mock_app import MockQubesComplete
 
 
 @pytest.fixture
 def test_qapp():
-    app = TestApp()
-    app.domains = TestVMCollection(
-        [
-            ('dom0', TestVM('dom0', klass='AdminVM', label='black',
-                            icon='adminvm-black', features={})),
-            ('test-vm',
-             TestVM('test-vm', klass='AppVM', label='blue', icon='appvm-blue',
-                    netvm=TestVM('sys-firewall'), template=TestVM('template'),
-                    features={})),
-            ('sys-firewall',
-             TestVM('sys-firewall', klass='DisposableVM', label='green',
-                    icon='servicevm-green', netvm=TestVM('sys-net'),
-                    template=TestVM('template'), features={})),
-            ('sys-net',
-             TestVM('sys-net', klass='StandaloneVM', label='red',
-                    icon='servicevm-red', provides_network=True,
-                    template=TestVM('template'), features={'servicevm': 1})),
-            ('template',
-             TestVM('template', klass='TemplateVM', label='red',
-                    icon='templatevm-red', features={})),
-            ('template-dvm',
-             TestVM('template-dvm', klass='AppVM', label='red',
-                    icon='templatevm-red', template_for_dispvms=True,
-                    netvm=TestVM('sys-net'), template=TestVM('template'),
-                    features={'appmenus-dispvm': 1})),
-        ]
-    )
-    return app
+    return MockQubesComplete()
