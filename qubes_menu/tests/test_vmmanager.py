673a4a5c8fdd0225d9f359cfdbd169b80f424097
Ben Grande <ben.grande.b@gmail.com>
2025-06-26 19:47:49 +0200

    Black formatter

--- qubes_menu/tests/test_vmmanager.py
+++ qubes_menu/tests/test_vmmanager.py
@@ -29,55 +29,66 @@ def test_vm_manager(test_qapp):
     dispatcher = qubesadmin.events.EventsDispatcher(test_qapp)
     vm_manager = VMManager(test_qapp, dispatcher)
 
-    entry_test = vm_manager.load_vm_from_name('test-vm')
+    entry_test = vm_manager.load_vm_from_name("test-vm")
     assert entry_test
-    assert entry_test.vm_name == 'test-vm'
-    assert entry_test.vm_icon_name == 'appvm-green'
-    entry_template = vm_manager.load_vm_from_name('fedora-36')
+    assert entry_test.vm_name == "test-vm"
+    assert entry_test.vm_icon_name == "appvm-green"
+    entry_template = vm_manager.load_vm_from_name("fedora-36")
     assert entry_template
     assert not entry_template.has_network
     assert not entry_test.is_dispvm_template
     assert not entry_test.service_vm
 
-    test_qapp._qubes['fedora-36'].properties['netvm'] = \
-        Property('sys-firewall', 'vm', False)
-    test_qapp._qubes['fedora-36'].update_calls()
+    test_qapp._qubes["fedora-36"].properties["netvm"] = Property(
+        "sys-firewall", "vm", False
+    )
+    test_qapp._qubes["fedora-36"].update_calls()
 
-    vm_manager._update_domain_property('fedora-36',
-                                       'property-set:netvm', name='netvm',
-                                       newvalue='sys-firewall',
-                                       oldvalue=None)
+    vm_manager._update_domain_property(
+        "fedora-36",
+        "property-set:netvm",
+        name="netvm",
+        newvalue="sys-firewall",
+        oldvalue=None,
+    )
     assert entry_template.has_network
 
-    test_qapp._qubes['test-vm'].properties['label'] = \
-        Property('red', 'label', False)
-    test_qapp._qubes['test-vm'].properties['icon'] = \
-        Property('appvm-red', 'str', False)
-    test_qapp._qubes['test-vm'].update_calls()
+    test_qapp._qubes["test-vm"].properties["label"] = Property(
+        "red", "label", False
+    )
+    test_qapp._qubes["test-vm"].properties["icon"] = Property(
+        "appvm-red", "str", False
+    )
+    test_qapp._qubes["test-vm"].update_calls()
 
-    vm_manager._update_domain_property('test-vm',
-                                       'property-set:label', name='label',
-                                       newvalue='red',
-                                       oldvalue='blue')
-    assert entry_test.vm_icon_name == 'appvm-red'
+    vm_manager._update_domain_property(
+        "test-vm",
+        "property-set:label",
+        name="label",
+        newvalue="red",
+        oldvalue="blue",
+    )
+    assert entry_test.vm_icon_name == "appvm-red"
 
-    test_qapp._qubes['test-vm'].properties['template_for_dispvms'] = \
-        Property('True', 'bool', False)
-    test_qapp._qubes['test-vm'].update_calls()
+    test_qapp._qubes["test-vm"].properties["template_for_dispvms"] = Property(
+        "True", "bool", False
+    )
+    test_qapp._qubes["test-vm"].update_calls()
 
-    vm_manager._update_domain_property('test-vm',
-                                       'property-set:template_for_dispvms',
-                                       name='template_for_dispvms',
-                                       newvalue=True)
+    vm_manager._update_domain_property(
+        "test-vm",
+        "property-set:template_for_dispvms",
+        name="template_for_dispvms",
+        newvalue=True,
+    )
     assert entry_test.is_dispvm_template
 
-    test_qapp._qubes['test-vm'].features['servicevm'] = 1
-    test_qapp._qubes['test-vm'].update_calls()
+    test_qapp._qubes["test-vm"].features["servicevm"] = 1
+    test_qapp._qubes["test-vm"].update_calls()
 
-    vm_manager._update_domain_feature('test-vm',
-                                      'feature-set:servicevm',
-                                      feature='servicevm',
-                                      value=1)
+    vm_manager._update_domain_feature(
+        "test-vm", "feature-set:servicevm", feature="servicevm", value=1
+    )
     assert entry_test.service_vm
 
 
@@ -85,10 +96,10 @@ def test_filter(test_qapp):
     dispatcher = qubesadmin.events.EventsDispatcher(test_qapp)
     vm_manager = VMManager(test_qapp, dispatcher)
 
-    entry_test = vm_manager.load_vm_from_name('test-vm')
-    entry_template = vm_manager.load_vm_from_name('fedora-36')
-    entry_service = vm_manager.load_vm_from_name('sys-net')
-    entry_dvm_template = vm_manager.load_vm_from_name('default-dvm')
+    entry_test = vm_manager.load_vm_from_name("test-vm")
+    entry_template = vm_manager.load_vm_from_name("fedora-36")
+    entry_service = vm_manager.load_vm_from_name("sys-net")
+    entry_dvm_template = vm_manager.load_vm_from_name("default-dvm")
     assert entry_test
     assert entry_template
     assert entry_service
