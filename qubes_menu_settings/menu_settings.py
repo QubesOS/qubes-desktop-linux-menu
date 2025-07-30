ef136cb6c3a843850e182799695e5daa2505a223
Marta Marczykowska-Górecka <marmarta@invisiblethingslab.com>
2025-07-30 16:24:27 +0200

    Blackify qubes menu settings

--- qubes_menu_settings/menu_settings.py
+++ qubes_menu_settings/menu_settings.py
@@ -29,30 +29,35 @@ import qubesadmin
 
 from qubes_config.widgets.gtk_widgets import ImageListModeler
 
-gi.require_version('Gtk', '3.0')
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
-from qubes_menu.constants import INITIAL_PAGE_FEATURE, SORT_RUNNING_FEATURE, \
-    POSITION_FEATURE, DISABLE_RECENT_FEATURE
+from qubes_menu.constants import (
+    INITIAL_PAGE_FEATURE,
+    SORT_RUNNING_FEATURE,
+    POSITION_FEATURE,
+    DISABLE_RECENT_FEATURE,
+)
 
 
 MENU_PAGES = {
     "search_page": "Search",
     "app_page": "Applications",
-    "favorites_page": "Favorites"
+    "favorites_page": "Favorites",
 }
 
 MENU_PAGES_DICT = {
     "Search": {"icon": "qappmenu-search", "object": "search_page"},
     "Applications": {"icon": "qappmenu-qube", "object": "app_page"},
-    "Favorites": {"icon": "qappmenu-favorites", "object": "favorites_page"}}
+    "Favorites": {"icon": "qappmenu-favorites", "object": "favorites_page"},
+}
 
 MENU_POSITIONS = {
     "top-left": "Top Left",
     "top-right": "Top Right",
     "bottom-left": "Bottom Left",
     "bottom-right": "Bottom Right",
-    "mouse": "Mouse"
+    "mouse": "Mouse",
 }
 
 MENU_POSITIONS_DICT = {
@@ -60,17 +65,20 @@ MENU_POSITIONS_DICT = {
     "Top Right": {"icon": "qappmenu-top-right", "object": "top-right"},
     "Bottom Left": {"icon": "qappmenu-bottom-left", "object": "bottom-left"},
     "Bottom Right": {"icon": "qappmenu-bottom-right", "object": "bottom-right"},
-    "Mouse": {"icon": "input-mouse-symbolic", "object": "mouse"}}
+    "Mouse": {"icon": "input-mouse-symbolic", "object": "mouse"},
+}
+
 
 class AppMenuSettings(Gtk.Application):
     """
     Qubes Menu Settings app.
     """
+
     def __init__(self, qapp: qubesadmin.Qubes):
         """
         :param qapp: qubesadmin.Qubes object
         """
-        super().__init__(application_id='org.qubesos.appmenusettings')
+        super().__init__(application_id="org.qubesos.appmenusettings")
         self.qapp = qapp
         self.vm = self.qapp.domains[self.qapp.local_name]
 
@@ -92,51 +100,57 @@ class AppMenuSettings(Gtk.Application):
         """
         self.builder = Gtk.Builder()
 
-        glade_path = (importlib.resources.files('qubes_menu_settings') /
-                      'menu_settings.glade')
+        glade_path = (
+            importlib.resources.files("qubes_menu_settings") / "menu_settings.glade"
+        )
         with importlib.resources.as_file(glade_path) as path:
             self.builder.add_from_file(str(path))
 
-        self.main_window : Gtk.ApplicationWindow = \
-            self.builder.get_object('main_window')
+        self.main_window: Gtk.ApplicationWindow = self.builder.get_object("main_window")
 
-        self.confirm_button: Gtk.Button = \
-            self.builder.get_object('button_confirm')
-        self.apply_button: Gtk.Button = self.builder.get_object('button_apply')
-        self.cancel_button: Gtk.Button = \
-            self.builder.get_object('button_cancel')
+        self.confirm_button: Gtk.Button = self.builder.get_object("button_confirm")
+        self.apply_button: Gtk.Button = self.builder.get_object("button_apply")
+        self.cancel_button: Gtk.Button = self.builder.get_object("button_cancel")
 
-        self.starting_page_combo: Gtk.ComboBox = \
-            self.builder.get_object("starting_page_combo")
+        self.starting_page_combo: Gtk.ComboBox = self.builder.get_object(
+            "starting_page_combo"
+        )
 
-        self.menu_position_combo: Gtk.ComboBox = \
-            self.builder.get_object("menu_position_combo")
+        self.menu_position_combo: Gtk.ComboBox = self.builder.get_object(
+            "menu_position_combo"
+        )
 
-        self.sort_running_check: Gtk.CheckButton = \
-            self.builder.get_object("sort_running_to_top_check")
+        self.sort_running_check: Gtk.CheckButton = self.builder.get_object(
+            "sort_running_to_top_check"
+        )
 
         self.show_recent_check: Gtk.CheckButton = self.builder.get_object(
-            "show_recent_apps_check")
+            "show_recent_apps_check"
+        )
         screen = Gdk.Screen.get_default()
         provider = Gtk.CssProvider()
 
-        css_path = (importlib.resources.files('qubes_menu_settings') /
-                      'menu_settings.css')
+        css_path = (
+            importlib.resources.files("qubes_menu_settings") / "menu_settings.css"
+        )
         with importlib.resources.as_file(css_path) as path:
             provider.load_from_path(str(path))
 
         Gtk.StyleContext.add_provider_for_screen(
-            screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+            screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
 
         self.confirm_button.connect("clicked", self._save_exit)
         self.apply_button.connect("clicked", self._save)
         self.cancel_button.connect("clicked", self._quit)
 
         self.initial_page_model = ImageListModeler(
-            self.starting_page_combo, MENU_PAGES_DICT)
+            self.starting_page_combo, MENU_PAGES_DICT
+        )
 
         self.menu_position_model = ImageListModeler(
-            self.menu_position_combo, MENU_POSITIONS_DICT)
+            self.menu_position_combo, MENU_POSITIONS_DICT
+        )
 
         self.load_state()
 
@@ -158,12 +172,10 @@ class AppMenuSettings(Gtk.Application):
         self.menu_position_model.select_name(MENU_POSITIONS[menu_position])
         self.menu_position_model.update_initial()
 
-        sort_running = \
-            bool(self.vm.features.get(SORT_RUNNING_FEATURE, False))
+        sort_running = bool(self.vm.features.get(SORT_RUNNING_FEATURE, False))
         self.sort_running_check.set_active(sort_running)
 
-        disable_recent = \
-            bool(self.vm.features.get(DISABLE_RECENT_FEATURE, False))
+        disable_recent = bool(self.vm.features.get(DISABLE_RECENT_FEATURE, False))
         self.show_recent_check.set_active(not disable_recent)
 
     def _quit(self, *_args):
@@ -189,19 +201,17 @@ class AppMenuSettings(Gtk.Application):
             if not old_sort_running:
                 self.vm.features[DISABLE_RECENT_FEATURE] = "1"
 
-        old_initial_page = self.vm.features.get(INITIAL_PAGE_FEATURE,
-                                                "app_page")
+        old_initial_page = self.vm.features.get(INITIAL_PAGE_FEATURE, "app_page")
 
         if self.initial_page_model.get_selected() != old_initial_page:
-            self.vm.features[INITIAL_PAGE_FEATURE] = \
+            self.vm.features[INITIAL_PAGE_FEATURE] = (
                 self.initial_page_model.get_selected()
+            )
 
-        old_menu_position = self.vm.features.get(POSITION_FEATURE,
-                                                "mouse")
+        old_menu_position = self.vm.features.get(POSITION_FEATURE, "mouse")
 
         if self.menu_position_model.get_selected() != old_menu_position:
-            self.vm.features[POSITION_FEATURE] = \
-                self.menu_position_model.get_selected()
+            self.vm.features[POSITION_FEATURE] = self.menu_position_model.get_selected()
 
     def _save_exit(self, *_args):
         self._save()
@@ -217,5 +227,5 @@ def main():
     app.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
